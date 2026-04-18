4c3b87dcdd2d66d7c154c2ab471e1fafdeb10518
Gabriel <testezin0028@gmail.com>
2026-04-18 13:12:08 -0300

    Media Finalizada

--- src/Media.java
+++ src/Media.java
@@ -2,23 +2,23 @@
 public class Media{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-         System.out.print("Programa para calculo de media das 4 Unidades");
+         System.out.println("Programa para calculo de media das 4 Unidades");
          double nota1, nota2, nota3, nota4;
          double media;
-         System.out.print("Digite a primeira Nota");
+         System.out.print("Digite a primeira Nota:");
          nota1 = scanner.nextDouble();
 
-         System.out.print("Digite a Segunda Nota");
+         System.out.print("Digite a Segunda Nota:");
          nota2 = scanner.nextDouble();
 
-         System.out.print("Digite a Tereira Nota");
+         System.out.print("Digite a Tereira Nota:");
          nota3 = scanner.nextDouble();
 
-         System.out.print("Digite a Quarta Nota");
+         System.out.print("Digite a Quarta Nota:");
          nota4 = scanner.nextDouble();
          
-         media = nota1 + nota2 + nota3 + nota4 / 4;
-         System.out.println("A media e", media);
+         media = (nota1 + nota2 + nota3 + nota4) / 4;
+         System.out.println("A media e:"+ media);
          scanner.close();
 
 
