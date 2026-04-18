1abdc6ff55938cf442764ca9326e85ea9ff39765
Gabriel <testezin0028@gmail.com>
2026-04-18 12:59:52 -0300

    Media

---
+++ src/Media.java
@@ -0,0 +1,29 @@
+  import java.util.Scanner;
+public class Media{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         System.out.print("Programa para calculo de media das 4 Unidades");
+         double nota1, nota2, nota3, nota4;
+         double media;
+         System.out.print("Digite a primeira Nota");
+         nota1 = scanner.nextDouble();
+
+         System.out.print("Digite a Segunda Nota");
+         nota2 = scanner.nextDouble();
+
+         System.out.print("Digite a Tereira Nota");
+         nota3 = scanner.nextDouble();
+
+         System.out.print("Digite a Quarta Nota");
+         nota4 = scanner.nextDouble();
+         
+         media = nota1 + nota2 + nota3 + nota4 / 4;
+         System.out.println("A media e", media);
+         scanner.close();
+
+
+        
+    }
+}
+
+
