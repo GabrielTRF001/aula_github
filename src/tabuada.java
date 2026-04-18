4052511d043db39a30aa440855f0601cd32fb3c3
Gabriel <testezin0028@gmail.com>
2026-04-18 11:50:33 -0300

    Tabudad Finalizada

--- src/tabuada.java
+++ src/tabuada.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
+
 public class tabuada {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um numero:");
+
+        System.out.println("Digite um número para ver a tabuada:");
         int numero = scanner.nextInt();
+
+        if (numero <= 0) {
+            System.out.println("Digite um número positivo!");
+        } else {
+
+            System.out.println("Tabuada do " + numero + ":");
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+        }
+
+        scanner.close();
     }
-    
 }
