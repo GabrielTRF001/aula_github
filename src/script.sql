269e322c261e0c3343b291b7df48a4c44bd84275
Gabriel <testezin0028@gmail.com>
2026-04-18 13:22:43 -0300

    Adicionando valores

--- src/script.sql
+++ src/script.sql
@@ -1,7 +1,11 @@
-create database_treinarecife;
+create database treinarecife;
 create table tb_alunos{
     id int auto_increment primary key,
     nome varchar(50) not null,
     email varchar(150) not null,
     data_nascimento datetime not null
 };
+insert into tb_alunos
+(id,nome,email,data_nascimento)
+values
+(1,"Alexandre de Souza JR", "alexandre@gmail.com",timestamp "1995-07-26");
